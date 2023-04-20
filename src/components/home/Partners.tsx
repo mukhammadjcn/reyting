c1f7123326fed77e273de52b082e85f64a6d497e
mukhammadjcn <mukhammadjcn@gmail.com>
2023-04-20 13:27:38 +0500

    changed images

--- src/components/home/Partners.tsx
+++ src/components/home/Partners.tsx
@@ -8,55 +8,63 @@ function Partners() {
       <div className="slider">
         <h2 className="section_title ">{t("home.partners")}</h2>
         <div className="slide-track">
-          <div className="slide">
+          <a href="https://www.ngmk.uz/ru" target="_blank" className="slide">
             <img src={require("src/assets/images/ytit.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://www.agmk.uz/oz" className="slide" target="_blank">
             <img src={require("src/assets/images/smart.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://cbu.uz/uz/" className="slide" target="_blank">
             <img src={require("src/assets/images/mb.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://aab.uz/uz/" className="slide" target="_blank">
             <img src={require("src/assets/images/aab.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://ai-journey.ru/" className="slide" target="_blank">
             <img src={require("src/assets/images/aij (2).png")} alt="" />
-          </div>
+          </a>
         </div>
         <div className="slide-track slide-track-right">
-          <div className="slide">
+          <a
+            href="https://ucell.uz/uz/subscribers"
+            className="slide"
+            target="_blank"
+          >
             <img src={require("src/assets/images/ucelll.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://uztelecom.uz/" className="slide" target="_blank">
             <img src={require("src/assets/images/uztelecom.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://uzinfocom.uz/uz" className="slide" target="_blank">
             <img src={require("src/assets/images/uzinfocom.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://tassvision.ai/" className="slide" target="_blank">
             <img src={require("src/assets/images/tass.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://zypl.ai/" className="slide" target="_blank">
             <img src={require("src/assets/images/zyplal.png")} alt="" />
-          </div>
+          </a>
         </div>
         <div className="slide-track">
-          <div className="slide">
+          <a href="https://tuit.uz/" className="slide" target="_blank">
             <img src={require("src/assets/images/tatu.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://amity.uz/" className="slide" target="_blank">
             <img src={require("src/assets/images/amity.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://tsul.uz/uz" className="slide" target="_blank">
             <img src={require("src/assets/images/yuridik.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a
+            href="https://www.zu.ac.ae/main/en/"
+            className="slide"
+            target="_blank"
+          >
             <img src={require("src/assets/images/zayed.png")} alt="" />
-          </div>
-          <div className="slide">
+          </a>
+          <a href="https://airi.uz/en" className="slide" target="_blank">
             <img src={require("src/assets/images/raqamli.png")} alt="" />
-          </div>
+          </a>
         </div>
       </div>
     </div>
